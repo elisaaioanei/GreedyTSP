19a7330cc325cc26fe51a07088b801672c81049f
Elisa Aioanei <elisa.aioanei@gmail.com>
2021-10-05 18:19:19 +0300

    Added backtrackig pseudocode

---
+++ GreedyTSP/BacktrackingSolution.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GreedyTSP
+{
+    public class BacktrackingSolution
+    {
+        public List<City> OrderedCities { get; set; }
+        public double TotalCost { get; set; }
+    }
+}
--- GreedyTSP/BacktrackingSolver.cs
+++ GreedyTSP/BacktrackingSolver.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace GreedyTSP
 {
@@ -13,6 +9,29 @@ namespace GreedyTSP
             var finalRoute = new List<City>();
 
             // To be implemented...
+
+            // Generate all order posibilities 
+            // Order 1-2-3-4...-10 => OrderedCities1
+            // Order 1-3-2-4-..-10 => OrderedCities2
+            // Order 1-4-2-3-..-10 => OrderedCities3
+
+            // var BacktrackingSolution1 = new BacktrackingSolution() {OrderedCities1, GetTotalCost(OrderedCities1)};
+            // var BacktrackingSolution2 = new BacktrackingSolution() {OrderedCities2, GetTotalCost(OrderedCities2)};
+            // var BacktrackingSolution3 = new BacktrackingSolution() {OrderedCities3, GetTotalCost(OrderedCities3)};
+
+
+            // var backtrackingSolutions = new List<BacktrackingSolution>(BacktrackingSolution1, BacktrackingSolution2, BacktrackingSolution3);
+
+            //var minCost = double.PositiveInfinity;
+            //for (int i = 0; i < backtrackingSolutions.Count; i++)
+            //{
+            //    if(minCost < backtrackingSolutions[i].TotalCost)
+            //    {
+            //        minCost = backtrackingSolutions[i].TotalCost;
+            //        finalRoute = backtrackingSolutions[i].OrderedCities;
+            //    }
+            //}
+
 
             return finalRoute;
         }
